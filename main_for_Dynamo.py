ca06e9fbce8d791161ca7fa688fb23e444504075
SlavaRevit <viacheslavgf@gmail.com>
2023-04-17 14:59:02 +0300

    changing main_for_Dynamo

--- main_for_Dynamo.py
+++ main_for_Dynamo.py
@@ -4,6 +4,7 @@ import clr
 import sys
 import pandas as pd
 import xlsxwriter
+
 localapp = os.getenv(r'LOCALAPPDATA')
 sys.path.append(os.path.join(localapp, r'python-3.8.3-embed-amd64\Lib\site-packages'))
 
@@ -181,9 +182,6 @@ def check_Volume_for_zero_down(result, floor_type_check):
             return volume
 
 
-
-
-
 """________BEAMS________"""
 
 beams_collector = FilteredElementCollector(doc). \
@@ -242,38 +240,39 @@ def check_for_zero(result, beam_type_check):
 """________FLOORS________"""
 
 floors_up_Area = {
-    "Regular": check_Area_for_zero(getting_Area_up(floors_collector, ["Regular", "Regular-T", "Balcon"]),
-                                   ["Regular", "Regular-T", "Balcon"]),
-    "Regular-P": check_Area_for_zero(getting_Area_up(floors_collector, ["Regular-P"]), ["Regular-P"]),
-    "Rampa": check_Area_for_zero(getting_Area_up(floors_collector, ["Rampa"]), ["Rampa"]),
+    "Up_Regular": check_Area_for_zero(getting_Area_up(floors_collector, ["Regular", "Regular-T", "Balcon"]),
+                                      ["Regular", "Regular-T", "Balcon"]),
+    "Up_Regular-P": check_Area_for_zero(getting_Area_up(floors_collector, ["Regular-P"]), ["Regular-P"]),
+    "Up_Rampa": check_Area_for_zero(getting_Area_up(floors_collector, ["Rampa"]), ["Rampa"]),
     "Regular-W Special": check_Area_for_zero(getting_Area_up(floors_collector, ["Regular-W Special"]),
                                              ["Regular-W Special"]),
     "Koteret": check_Area_for_zero(getting_Area_up(floors_collector, ["Koteret"]), ["Koteret"]),
     "Lite Beton": check_Area_for_zero(getting_Area_up(floors_collector, ["Lite Beton"]), ["Lite Beton"]),
     "Geoplast-D": check_Area_for_zero(getting_Area_up(floors_collector, ["Geoplast-D"]), ["Geoplast-D"]),
-    "Slab-Rib Special": check_Area_for_zero(getting_Area_up(floors_collector, ["Slab-Rib Special"]), ["Slab-Rib Special"]),
+    "Slab-Rib Special": check_Area_for_zero(getting_Area_up(floors_collector, ["Slab-Rib Special"]),
+                                            ["Slab-Rib Special"]),
     "CLSM": check_Area_for_zero(getting_Area_up(floors_collector, ["CLSM"]), ["CLSM"])
 }
 
 floors_down_Area = {
-    "Regular": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular", "Regular-T", "Balcon"]),
-                                        ["Regular", "Regular-T", "Balcon"]),
-    "Regular-P": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular-P"]), ["Regular-P"]),
-    "Rampa": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Rampa"]), ["Rampa"]),
-    "Regular-W Special": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular-W Special"]),
-                                                  ["Regular-W Special"]),
-    "Koteret": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Koteret"]), ["Koteret"]),
-    "Lite Beton": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Lite Beton"]), ["Lite Beton"]),
-    "Geoplast-D": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Geoplast-D"]), ["Geoplast-D"]),
-    "Slab-Rib Special": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Slab-Rib Special"]),
-                                                 ["Slab-Rib Special"]),
+    "Dn_Regular": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular", "Regular-T", "Balcon"]),
+                                           ["Regular", "Regular-T", "Balcon"]),
+    "Dn_Regular-P": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular-P"]), ["Regular-P"]),
+    "Dn_Rampa": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Rampa"]), ["Rampa"]),
+    "Dn_Regular-W Special": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Regular-W Special"]),
+                                                     ["Regular-W Special"]),
+    "Dn_Koteret": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Koteret"]), ["Koteret"]),
+    "Dn_Lite Beton": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Lite Beton"]), ["Lite Beton"]),
+    "Dn_Geoplast-D": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Geoplast-D"]), ["Geoplast-D"]),
+    "Dn_Slab-Rib Special": check_Area_for_zero_down(getting_Area_down(floors_collector, ["Slab-Rib Special"]),
+                                                    ["Slab-Rib Special"]),
     "CLSM": check_Area_for_zero_down(getting_Area_down(floors_collector, ["CLSM"]), ["CLSM"])
 }
 floors_up_Volume = {
-    "Regular": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Regular", "Regular-T", "Balcon"]),
-                                     ["Regular", "Regular-T", "Balcon"]),
-    "Regular-P": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Regular-P"]), ["Regular-P"]),
-    "Rampa": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Rampa"]), ["Rampa"]),
+    "Up_Regular": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Regular", "Regular-T", "Balcon"]),
+                                        ["Regular", "Regular-T", "Balcon"]),
+    "Up_Regular-P": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Regular-P"]), ["Regular-P"]),
+    "Up_Rampa": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Rampa"]), ["Rampa"]),
     "Regular-W Special": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Regular-W Special"]),
                                                ["Regular-W Special"]),
     "Koteret": check_Volume_for_zero(getting_Volume_up(floors_collector, ["Koteret"]), ["Koteret"]),
@@ -285,17 +284,17 @@ floors_up_Volume = {
 }
 
 floors_down_Volume = {
-    "Regular": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular", "Regular-T", "Balcon"]),
-                                          ["Regular", "Regular-T", "Balcon"]),
-    "Regular-P": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular-P"]), ["Regular-P"]),
-    "Rampa": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Rampa"]), ["Rampa"]),
-    "Regular-W Special": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular-W Special"]),
-                                                    ["Regular-W Special"]),
-    "Koteret": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Koteret"]), ["Koteret"]),
-    "Lite Beton": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Lite Beton"]), ["Lite Beton"]),
-    "Geoplast-D": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Geoplast-D"]), ["Geoplast-D"]),
-    "Slab-Rib Special": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Slab-Rib Special"]),
-                                                   ["Slab-Rib Special"]),
+    "Dn_Regular": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular", "Regular-T", "Balcon"]),
+                                             ["Regular", "Regular-T", "Balcon"]),
+    "Dn_Regular-P": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular-P"]), ["Regular-P"]),
+    "Dn_Rampa": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Rampa"]), ["Rampa"]),
+    "Dn_Regular-W Special": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Regular-W Special"]),
+                                                       ["Regular-W Special"]),
+    "Dn_Koteret": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Koteret"]), ["Koteret"]),
+    "Dn_Lite Beton": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Lite Beton"]), ["Lite Beton"]),
+    "Dn_Geoplast-D": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Geoplast-D"]), ["Geoplast-D"]),
+    "Dn_Slab-Rib Special": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["Slab-Rib Special"]),
+                                                      ["Slab-Rib Special"]),
     "CLSM": check_Volume_for_zero_down(getting_Volume_down(floors_collector, ["CLSM"]), ["CLSM"])
 }
 
@@ -313,26 +312,26 @@ beams = {
     "beam_precast": check_for_zero(getting_Count(beams_collector, ["Precast"]), ["Precast"]),
 }
 
-
 """________Walls________"""
 
-wall_collector = FilteredElementCollector(doc).\
-    OfCategory(BuiltInCategory.OST_Walls).\
-    WhereElementIsNotElementType().\
+wall_collector = FilteredElementCollector(doc). \
+    OfCategory(BuiltInCategory.OST_Walls). \
+    WhereElementIsNotElementType(). \
     ToElementIds()
 walls_Out = []
 walls_In = []
 for wall_t in wall_collector:
-    #geting element from wall.id
+    # geting element from wall.id
     new_w = doc.GetElement(wall_t)
     # going to WallType
     wall_type = new_w.WallType
-    #getting type_comments of this wall
+    # getting type_comments of this wall
     wall_type_comments = wall_type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_COMMENTS).AsString()
     if wall_type_comments == "FrOut":
         walls_Out.append(new_w)
     elif wall_type_comments == "FrIN":
         walls_In.append(new_w)
+
 
 def getting_Volume_Area_Out(walls_list):
     total_Area_Out = 0.0
@@ -356,43 +355,42 @@ def getting_Volume_Area_IN(walls_list):
     return total_Area_In, total_Volume_In
 
 
-
 walls = {
-    "walls_In" : getting_Volume_Area_IN(wall_collector),
-    "walls_Out" : getting_Volume_Area_Out(wall_collector)
+    "walls_In": getting_Volume_Area_IN(wall_collector),
+    "walls_Out": getting_Volume_Area_Out(wall_collector)
 }
 
-
-"""________Creating_Excel_File________""" # attempt 1
+"""________Creating_Excel_File________"""  # attempt 1
 
 # creating data_frama of beams from dict
 df_beams = pd.DataFrame.from_dict(beams, orient="index", columns=["Value"])
+df_beams = df_beams.dropna()
 df_walls = pd.DataFrame.from_dict(walls, orient="index", columns=["Area", "Volume"])
 
 # from tuples spliting result to 2 different columns, column1 = Volume, column2 = Count
 df_beams[['Column1', 'Column2']] = df_beams['Value'].apply(
-    lambda x: pd.Series([x[0], x[1]] if isinstance(x, tuple) else [x, None]))
-df_beams = df_beams.drop(columns=['Value']).rename(columns={'Column1': 'Volume', 'Column2': 'Count'})
+    lambda x: pd.Series([x[0], x[1]] if isinstance(x, tuple) and len(x) >= 1 else [x, None]))
 
+df_beams_result = df_beams.drop(columns=['Value']).rename(columns={'Column1': 'Volume', 'Column2': 'Count'})
 
 """Series for floor Area/Volume UP"""
 floors_up_Area_filtered = {k: v for k, v in floors_up_Area.items() if v}
 area_up_series = pd.Series(floors_up_Area_filtered, name="Area")
 floors_up_Volume_filtered = {k: v for k, v in floors_up_Volume.items() if v}
 volume_up_series = pd.Series(floors_up_Volume_filtered, name="Volume")
-df_floors_up = pd.concat([area_up_series,volume_up_series], axis=1)
+df_floors_up = pd.concat([area_up_series, volume_up_series], axis=1)
 
 """Series for floor Area/Volume DOWN"""
 floors_up_Area_filtered = {k: v for k, v in floors_down_Area.items() if v}
 area_down_series = pd.Series(floors_up_Area_filtered, name="Area")
 floors_up_Volume_filtered = {k: v for k, v in floors_down_Volume.items() if v}
 volume_down_series = pd.Series(floors_up_Volume_filtered, name="Volume")
-df_floors_down = pd.concat([area_down_series,volume_down_series], axis=1)
+df_floors_down = pd.concat([area_down_series, volume_down_series], axis=1)
 
 # Inserting new Name of Type:
 
-name_up = "Floors_Up"
-df_name_floors_up = pd.DataFrame(index=["Floors_Up"])
+name_up = "Floors"
+df_name_floors_up = pd.DataFrame(index=["Floors"])
 name_down = "Floors_Down"
 df_name_floors_down = pd.DataFrame(index=["Floors_Down"])
 name_beams = "Beams"
@@ -404,7 +402,14 @@ df_name_walls = pd.DataFrame(index=['Walls'])
 # df = pd.concat([df_name_beams, df_beams, df_name_floors, df_floors], axis=0, sort=False)
 # df = pd.concat([df_name_beams,df_beams,df_name_floors,df_floors_up_GENERAL,df_name_floors,df_floors_area_down,df_floors_volume_down,df_name_walls,df_walls ], axis=0, sort=False)
 
-df = pd.concat([df_name_beams,df_beams,df_name_floors_up,df_floors_up,df_name_floors_down,df_floors_down,df_name_walls,df_walls], axis=0, sort=False).round(2)
+df = pd.concat(
+    [df_name_beams, df_beams_result, df_name_floors_up, df_floors_up, df_floors_down, df_name_walls, df_walls], axis=0,
+    sort=False).round(2)
+col_1_sum = df['Area'].sum()
+col_2_sum = df['Volume'].sum()
+total_row = pd.Series({"Area":col_1_sum, "Volume":col_2_sum,"Count" : ""},name="Total")
+df = df._append(total_row)
+
 
 
 df = df.fillna('')
@@ -414,4 +419,3 @@ df = df.reindex(columns=["Area", "Volume", "Count"])
 writer = pd.ExcelWriter(IN[1], engine='xlsxwriter')
 df.to_excel(writer, sheet_name="Test")
 writer._save()
-
